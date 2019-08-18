99fff77447f08a3bc94299dc6c06cec8eb162f0f
Milan Rafaeli <mbrafaeli@uc.cl>
2019-08-18 15:58:31 -0400

    feat(user-sign-up) connection to devise stores login and register

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/show'
   get 'landing/show'
-  get 'store/login'
   match '/search', to: 'landing#search', via: 'post'
   root to: 'home#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
