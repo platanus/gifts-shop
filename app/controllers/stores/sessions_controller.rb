28591fe420ff8c880373a3668ef49d5eaf2c320a
Milan Rafaeli <mbrafaeli@uc.cl>
2019-08-19 17:14:40 -0400

    refactor(store-register) remove views and controllers generated by devise, set parameters sanitizer on registration_controller

--- app/controllers/stores/sessions_controller.rb
+++ app/controllers/stores/sessions_controller.rb
@@ -1,29 +1,5 @@
-# frozen_string_literal: true
-
-class Stores::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-  def after_sign_in_path_for(*)
-    catalog_path
+class Stores::SessionsController < Devise::RegistrationsController
+  def after_sign_in_path_for(_resource)
+    stores_catalog_show_path
   end
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
