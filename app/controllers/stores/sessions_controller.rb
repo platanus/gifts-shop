29fff0daeee8143663e6bdfd6c2137500db0c1d2
Milan Rafaeli <mbrafaeli@uc.cl>
2019-08-21 10:15:19 -0400

    bug(user-login) fix Session Controller parent name

--- app/controllers/stores/sessions_controller.rb
+++ app/controllers/stores/sessions_controller.rb
@@ -1,5 +1,14 @@
-class Stores::SessionsController < Devise::RegistrationsController
+class Stores::SessionsController < Devise::SessionsController
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(_resource)
     stores_catalog_show_path
+  end
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
 end
