8ef5e02bc7a5b92750774ca001bc642c68af8847
hbkim94 <hbkim@uc.cl>
2019-08-28 17:09:45 -0400

    feat(home): manage Home view through Vue

---
+++ app/javascript/src/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    id="app"
+    class="home-main"
+  >
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+  @import '../styles/variables';
+
+  .home-main {
+    display: flex;
+    align-self: center;
+    justify-content: center;
+    overflow-y: scroll;
+    font-family: 'Work Sans', sans-serif;
+    height: 100vh;
+    font-size: 11px;
+  }
+</style>
