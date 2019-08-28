37e88747288ba62f5c12ddd199c379d4aee28bc1
Milan Rafaeli <mbrafaeli@uc.cl>
2019-08-28 10:31:45 -0400

    feat (new-product) add new product page and basic layout

--- app/controllers/stores/catalog_controller.rb
+++ app/controllers/stores/catalog_controller.rb
@@ -1,7 +1,17 @@
 class Stores::CatalogController < ApplicationController
   before_action :authenticate_store!
 
+  def create; end
+
   def show
     @products = current_store.products
+  end
+
+  def new; end
+
+  private
+
+  def post_params
+    params.require(:product).permit(:name, :link, :price, :image)
   end
 end
