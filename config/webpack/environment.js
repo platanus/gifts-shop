23cee85988a32de7ff4ec496710aacfa3a56bea2
Hue Bin Kim <hbkim@uc.cl>
2019-09-04 16:53:03 -0300

    feat(vue): add sass loader for styles within vue components

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,10 @@
-const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
+const { environment } = require('@rails/webpacker');
+const { VueLoaderPlugin } = require('vue-loader');
+const vue = require('./loaders/vue');
+const sass = require('./loaders/sass');
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-module.exports = environment
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin());
+environment.loaders.prepend('vue', vue);
+environment.loaders.append('sass', sass);
+
+module.exports = environment;
