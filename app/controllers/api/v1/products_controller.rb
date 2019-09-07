f8921d6fd90358ce43c21d5e7efd6d539efb3d8d
jgmontoya <jgmontoya@pm.me>
2019-09-07 13:44:17 -0400

    fix(api/products): return products in default order if recommender url is not set

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -9,6 +9,8 @@ class Api::V1::ProductsController < Api::V1::BaseController
   private
 
   def recommender_sort(products)
+    return products if ENV['RECOMMENDER_URL'].blank?
+
     number_of_products = products.length
     uri = URI.parse("#{ENV['RECOMMENDER_URL']}/recommend/#{number_of_products}")
     begin
