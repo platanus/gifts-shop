2aaf80f32c6fd14f51a52238c46967c1b270c39d
jgmontoya <jgmontoya@pm.me>
2019-09-07 13:01:56 -0400

    feat(api): sort products by recommender order

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,6 +2,17 @@
 
 class Api::V1::ProductsController < Api::V1::BaseController
   def index
-    render json: Product.all
+    products = recommender_sort(Product.all)
+    render json: products
+  end
+
+  private
+
+  def recommender_sort(products)
+    number_of_products = products.length
+    uri = URI.parse("#{ENV['RECOMMENDER_URL']}/recommend/#{number_of_products}")
+    response = Net::HTTP.get_response(uri)
+    order = JSON.parse(response.body)['product_ids']
+    products.sort_by { |product| order.index(product.id) }
   end
 end
