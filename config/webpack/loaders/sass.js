702d14aabcd0f8b066a8ab9d3655c6ed8b54be90
Hue Bin Kim <hbkim@uc.cl>
2019-09-09 14:26:50 -0300

    fix(webpack): configure css-loader module for heroku build bug

--- config/webpack/loaders/sass.js
+++ config/webpack/loaders/sass.js
@@ -1,9 +1,21 @@
 module.exports = {
-  test: /\.scss$/,
-  use: [
-    'vue-style-loader',
-    'css-loader',
-    'sass-loader',
-  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'vue-style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
+          },
+          { loader: 'sass-loader' },
+        ],
+      },
+    ],
+  },
 };
   
