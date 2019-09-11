386581844d1fe12350519da82d03694f1e9cf514
Hue Bin Kim <hbkim@uc.cl>
2019-09-11 15:07:00 -0300

    Revert "fix(webpack): fix wrong loader name to correct one"
    This reverts commit 055beab5f13e11d9502f68bc3769a893dff7797f.

--- config/webpack/loaders/sass.js
+++ config/webpack/loaders/sass.js
@@ -1,20 +1,8 @@
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          { loader: 'vue-style-loader' },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]',
-            },
-          },
-          { loader: 'sass-loader' },
-        ],
-      },
-    ],
-  },
+  test: /\.scss$/,
+  use: [
+      'vue-style-loader',
+      'css-style-loader',
+      'sass-loader',
+  ],
 };
