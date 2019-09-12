6012509f1ee68e0661162530f0e08c4a29ca8da8
jgmontoya <jgmontoya@pm.me>
2019-09-12 18:07:45 -0300

    refactor(): use recommendation command instead of service

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::ProductsController < Api::V1::BaseController
+  before_action :redirect_to_landing_if_receiver_not_valid, only: :index
+
   def index
-    recommender_service = RecommenderService.new
-    products = recommender_service.get_all_products
+    products = GetProductsRecommendation.for(cookies[:receiver_id], 5)
     render json: products
+  end
+
+  private
+
+  def redirect_to_landing_if_receiver_not_valid
+    redirect_to landing_show_path unless receiver_is_valid?
+  end
+
+  def receiver_is_valid?
+    Receiver.find(cookies[:receiver_id]).where(giver_id: cookies[:giver_id]).any?
   end
 end
