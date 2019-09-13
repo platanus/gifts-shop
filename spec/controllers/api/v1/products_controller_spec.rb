7fb0ca82b85e0dbb17d2296ea46b0852540f6ef1
jgmontoya <jgmontoya@pm.me>
2019-09-13 11:41:37 -0300

    test(api/products): update spec to include session contexts

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -2,12 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::ProductsController, type: :controller do
   describe "GET #index" do
-    before do
-      get :index
+    context 'with proper session' do
+      let(:receiver) { create(:receiver) }
+      let(:giver) { receiver.giver }
+
+      before do
+        request.cookies['receiver_id'] = receiver.id
+        request.cookies['giver_id'] = giver.id
+        get :index, format: :json
+      end
+
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
     end
 
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
+    context 'without proper session' do
+      before do
+        get :index, format: :json
+      end
+
+      it 'redirects to landing page' do
+        expect(response).to redirect_to landing_show_path
+      end
     end
   end
 end
