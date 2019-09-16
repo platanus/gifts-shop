4b97fa23e5bd219db83eea6c7af8f6d3e600cad0
Hue Bin Kim <hbkim@uc.cl>
2019-09-16 18:09:09 -0300

    refactor(api): modify ProductAction creation of type 'displayed'

--- app/controllers/api/v1/product_actions_controller.rb
+++ app/controllers/api/v1/product_actions_controller.rb
@@ -1,25 +1,25 @@
 class Api::V1::ProductActionsController < Api::V1::BaseController
   def create
-    mark_displayed
+    if permitted_params[:action_type] == 'display'
+      respond_with mark_displayed
+    else
+      puts 'Pendiente para like'
+    end
   end
 
   private
 
   def mark_displayed
     receiver_id = set_receiver_id
-    ActiveRecord::Base.transaction do
-      permitted_params[:products].each do |_key, value|
-        ProductAction.create!(
-          receiver_id: receiver_id,
-          product_id: value[:id],
-          action_type: 0
-        )
-      end
-    end
+    ProductAction.create!(
+      receiver_id: receiver_id,
+      product_id: permitted_params[:product_id],
+      action_type: permitted_params[:action_type]
+    )
   end
 
   def permitted_params
-    params.permit(products: {})
+    params.permit(:product_id, :action_type)
   end
 
   def set_receiver_id
