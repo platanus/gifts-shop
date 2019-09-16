f9376a0282c8478d203644fdd4bcf9aff9c671a8
Hue Bin Kim <hbkim@uc.cl>
2019-09-16 13:09:21 -0300

    feat(product): adds method that visually sets a liked state on a product

--- app/javascript/src/components/product.vue
+++ app/javascript/src/components/product.vue
@@ -15,10 +15,20 @@
       </div>
       <div class="home-product__bottom-row-container">
         <span class="home-product__price"> {{ product.price | Price }} </span>
-        <img
-          class="home-product__icon"
-          src="../assets/heart-outlined.svg"
+        <div 
+          @click="setLikeStatus" 
         >
+          <img
+            v-if="liked"
+            class="home-product__icon"
+            :src="heartFilled"
+          >
+          <img
+            v-else
+            class="home-product__icon"
+            :src="heartOutlined"
+          >
+        </div>
       </div>
     </div>
   </div>
@@ -28,10 +38,28 @@
 import convertToClp from '../utils/convert-to-clp';
 
 export default {
+  data() {
+    return {
+      liked: false,
+      heartFilled: "/packs/media/src/assets/heart-filled-bf0c782fd35f9d63ac1a07f730aad400.svg",
+      heartOutlined: "/packs/media/src/assets/heart-outlined-6a8e3c8eb2ebab409cd726c91cf14777.svg",
+    };
+  },
   props: {
     product: {
       type: Object,
       default: null,
+    },
+  },
+  methods: {
+    setLikeStatus() {
+      if (this.liked) {
+        this.liked = false;
+        console.log("primer if");
+      } else {
+        this.liked = true;
+        console.log("segundo")
+      }
     },
   },
   filters: {
@@ -88,6 +116,7 @@ export default {
 
     &__icon {
       width: 1.7em;
+      cursor: pointer;
     }
 
     &__title {
