80f19ddef8185f715fb33d5a09aa6dcf8e7e852f
Milan Rafaeli <mbrafaeli@uc.cl>
2019-10-16 15:32:41 -0300

    feat(promoted-click): save promoted click action record

--- app/controllers/api/v1/product_actions_controller.rb
+++ app/controllers/api/v1/product_actions_controller.rb
@@ -1,17 +1,29 @@
 class Api::V1::ProductActionsController < Api::V1::BaseController
   def create
-    product_action = ProductAction.create!(
-      receiver_id: receiver_id,
-      product_id: permitted_params[:product_id],
-      action_type: permitted_params[:action_type]
-    )
+    product_action = save_action(permitted_params[:action_type])
+    save_promoted if permitted_params[:action_type] == 'click'
     respond_with product_action
   end
 
   private
 
+  def save_action(action_type)
+    ProductAction.create!(
+      receiver_id: receiver_id,
+      product_id: permitted_params[:product_id],
+      action_type: action_type
+    )
+  end
+
   def permitted_params
     params.require(:product_action).permit(:product_id, :action_type)
+  end
+
+  def save_promoted
+    product = Product.find_by(id: permitted_params[:product_id])
+    if product&.promoted && product&.store&.has_enough_balance?
+      save_action('promoted_click')
+    end
   end
 
   def receiver_id
