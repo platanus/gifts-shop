9cd8096eeb1008b48be83bcf9c354c521f3ceb74
Javier <jgmontoya@pm.me>
2019-10-16 14:48:21 -0300

    refactor(): remove receivers reference migration

--- db/migrate/20190807203700_add_receivers_reference.rb
+++
@@ -1,7 +0,0 @@
-class AddReceiversReference < ActiveRecord::Migration[5.2]
-  disable_ddl_transaction!
-  def change
-    add_reference :reminders, :receivers, index: false
-    add_index :reminders, :receivers_id, algorithm: :concurrently
-  end
-end
