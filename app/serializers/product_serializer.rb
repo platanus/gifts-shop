2fd67142b6b1ac801da6154e20c16b5a81c49ba4
Milan Rafaeli <mbrafaeli@uc.cl>
2019-10-17 11:19:22 -0300

    feat(serializer): get promoted attriubute from serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,10 +1,14 @@
 class ProductSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :name, :price, :store_name, :image_url, :link
+  attributes :id, :name, :price, :store_name, :image_url, :link, :promoted
 
   def store_name
     object.store.name
+  end
+
+  def promoted
+    object.store.has_enough_balance? && object.promoted
   end
 
   def image_url
