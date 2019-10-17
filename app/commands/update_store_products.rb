42ebb2916da58dc150ea30724ef83d32132b43ba
Javier <jgmontoya@pm.me>
2019-10-17 16:38:25 -0300

    fix(): skip problematic products in csv

--- app/commands/update_store_products.rb
+++ app/commands/update_store_products.rb
@@ -2,13 +2,16 @@ class UpdateStoreProducts < PowerTypes::Command.new(:store, :csv_url)
   def perform
     @store.products.destroy_all
     products.each do |params|
-      product = Product.create!(
+      if product = Product.create(
         name: params.name,
         link: params.link,
         price: params.price,
         store_id: @store.id
       )
-      product.attach_image_from_url(params.image)
+        product.attach_image_from_url(params.image)
+      end
+    rescue OpenURI::HTTPError
+      next
     end
   end
 
