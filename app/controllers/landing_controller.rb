7a8a15990cb2bd06a5eda14b23c9e0b2d8f8d7d2
Diego Casas <dacasas@uc.cl>
2019-10-24 13:11:41 -0300

    fix(landing): set giver from cookies if the same

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,6 @@
 class LandingController < ApplicationController
   before_action :check_for_receiver, only: :show
+  before_action :set_giver, only: :search
 
   DEFAULT_REGION = ENV.fetch('DEFAULT_REGION', 1).to_i
   def show; end
@@ -25,8 +26,12 @@ class LandingController < ApplicationController
     search if params[:name]
   end
 
+  def set_giver
+    @giver = Giver.find_by(id: cookies[:giver_id])
+  end
+
   def create_giver_and_receiver
-    @giver = Giver.create!(region_id: DEFAULT_REGION)
+    @giver = Giver.create!(region_id: DEFAULT_REGION) if @giver.blank?
     @receiver = Receiver.create!(
       name: params[:name],
       giver_id: @giver.id
