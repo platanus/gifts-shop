d9252bbde152a5935a6ad372085e6a3cd031c2cd
Diego Casas <dacasas@uc.cl>
2019-10-24 10:35:07 -0300

    style(header): center titles style

--- app/javascript/src/components/header-center.vue
+++ app/javascript/src/components/header-center.vue
@@ -10,14 +10,17 @@
     </div>
     <div class="header-center__powerbar">
       <img
-        class="header-center__icon header-center__icon--title"
+        class="header-center__icon header-center__icon--powerbar"
         src="../assets/bot.svg"
       >
       <div class="powerbar__background">
         <div class="powerbar__foreground" />
       </div>
     </div>
-    <div class="header-center__subtitle">
+    <div
+      class="header-center__subtitle"
+      :class="{ 'header-center__subtitle--zero-height': !onTop}"
+    >
       Aumenta el poder con
       <img
         class="header-center__icon header-center__icon--subtitle"
@@ -67,18 +70,39 @@ export default {
     &__title {
       flex-direction: row;
       align-items: center;
-      padding: .15em 0;
+      font-weight: 300;
+      width: 100%;
+      text-align: center;
+      margin: .15em 0;
       color: $title-font-color;
+
+      &--emphasis-word {
+        font-style: italic;
+        color: $emphasis-word-font-color;
+      }
     }
 
     &__subtitle {
       color: $subtitle-font-color;
-      font-weight: 300;
-    }
+      opacity: .56;
+      font-size: .9em;
+      text-align: center;
+      transition: all .5s;
+      height: 1em;
+      overflow: hidden;
 
-    &__emphasis-word {
-      font-style: italic;
-      color: $emphasis-word-font-color;
+      &--zero-height {
+        height: 0;
+      }
+
+      @media (min-width: $p-break) {
+        text-align: right;
+        height: fit-content;
+
+        &--zero-height {
+          height: fit-content;
+        }
+      }
     }
 
     &__user-name {
@@ -88,6 +112,27 @@ export default {
 
     &__icon {
       align-self: flex-end;
+      margin-bottom: -.1em;
+
+      &--powerbar {
+        margin: 0 1em .3em 0;
+        width: 2.3em;
+        height: 2.3em;
+      }
+
+      &--title {
+        border-radius: 50%;
+        box-shadow: 2px 2px 3px #bbb;
+
+        &:hover {
+          cursor: pointer;
+        }
+      }
+
+      &--subtitle {
+        width: 1em;
+        height: 1em;
+      }
     }
   }
 </style>
