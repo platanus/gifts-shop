bd2a5f95adb6b62b35410de39494ab25e83d6ab0
Diego Casas <dacasas@uc.cl>
2019-12-05 09:49:54 -0300

    feat(AA/Product): add image field to edit form, with hint

--- app/admin/store_products.rb
+++ app/admin/store_products.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Product do
   menu parent: I18n.t('activeadmin.titles.stores')
 
-  permit_params :store, :name, :price, :link, :promoted, :display, :gender, :age, :novelty
+  permit_params :store, :name, :price, :link, :promoted, :display, :gender, :age, :novelty, :image
 
   filter :name
   filter :store
@@ -30,6 +30,9 @@ ActiveAdmin.register Product do
       f.input :name
       f.input :price
       f.input :link
+      image = f.object.image
+      hint = image.attached? ? image_tag(url_for(image)) : content_tag(:span, "Sin imagen")
+      f.input :image, as: :file, hint: hint
       f.input :promoted
       f.input :display
       f.input :gender
