69ea3b09f131800e2be889bbe5ba4a3838c9d072
jgmontoya <jgmontoya@pm.me>
2019-12-18 15:42:22 -0300

    test(feedback): add receiver and response validations
    It also changes the description to context

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Feedback, type: :model do
-  it 'is valid with valid attributes' do
-    expect(build(:feedback)).to be_valid
+  describe 'validations' do
+    it { is_expected.to belong_to(:receiver) }
+    it { is_expected.to validate_presence_of(:response) }
   end
-  it 'is not valid without a response' do
-    expect(build(:feedback, response: nil)).not_to be_valid
+
+  context 'with valid attributes' do
+    it { expect(build(:feedback)).to be_valid }
   end
-  it 'is not valid without a receiver' do
-    expect(build(:feedback, receiver_id: nil)).not_to be_valid
+
+  context 'without a response' do
+    it { expect(build(:feedback, response: nil)).not_to be_valid }
+  end
+
+  context 'without a receiver' do
+    it { expect(build(:feedback, receiver_id: nil)).not_to be_valid }
   end
 end
