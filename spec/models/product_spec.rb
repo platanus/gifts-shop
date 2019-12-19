2d3fc4ca3f2af80ffb021aeeb60e4be1e78e4a64
Milan Rafaeli <mbrafaeli@uc.cl>
2019-12-19 17:19:48 -0300

    test(product-spec): add product validation tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:product)).to be_valid
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_numericality_of(:price).is_greater_than_or_equal_to(1) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:link) }
+  end
 end
