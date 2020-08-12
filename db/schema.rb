b3ee48d4f03e661a1d925e8c70c39a62a3258476
Milan Rafaeli <milan.rafaeli@gmail.com>
2020-08-12 09:52:03 -0400

    feat(product): add status attribute to product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_21_170336) do
+ActiveRecord::Schema.define(version: 2020_08_12_133529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -174,6 +174,7 @@ ActiveRecord::Schema.define(version: 2019_12_21_170336) do
     t.integer "gender", default: 0
     t.integer "age", default: 0
     t.integer "novelty"
+    t.string "status"
     t.index ["store_id"], name: "index_products_on_store_id"
   end
 
