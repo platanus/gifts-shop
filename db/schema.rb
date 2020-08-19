0bc61242dde44c6a634f3c801ab17951df9d22c5
sleivav <sergio.leiva.ocl@gmail.com>
2020-08-19 15:26:13 -0400

    fix(db): restore mistakenly removed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_133529) do
+ActiveRecord::Schema.define(version: 2020_08_17_213636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,15 @@ ActiveRecord::Schema.define(version: 2020_08_12_133529) do
     t.date "deposit_time", null: false
     t.index ["organization_id"], name: "index_deposits_on_organization_id"
     t.index ["store_id"], name: "index_deposits_on_store_id"
+  end
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.bigint "receiver_id"
+    t.text "additional_info"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "fulfilled"
+    t.index ["receiver_id"], name: "index_feedbacks_on_receiver_id"
   end
 
   create_table "ledgerizer_accounts", force: :cascade do |t|
