4e9704ecbc65a0c4b5c61839b300ce12e5d06a3a
ankaph <ankaph@protonmail.com>
2020-08-27 12:26:11 -0400

    test(product): update product states test

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -4,8 +4,12 @@ FactoryBot.define do
     price { 1.5 }
     clicks { "" }
     link { "MyString" }
+    email { "MyString" }
     clicks_cost { 1.5 }
     store
     novelty { 3 }
+    trait :with_image do
+      image { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files','example.jpg'), 'image/jpg') }
+    end
   end
 end
