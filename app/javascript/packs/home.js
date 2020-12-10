a96bdffd765b4935a2a5186aa437f8fb5c70afc0
pfarellano <pfarellano@uc.cl>
2020-12-10 12:15:38 -0300

    feat(notification): add component for notifications

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import router from '../src/router';
 import store from '../src/store/index';
 import app from '../src/app';
+import Notification from '../src/utils/notification';
 
 import '../styles/application.css';
+
+Vue.use(Notification);
 
 document.addEventListener('DOMContentLoaded', () => {
   if (document.getElementById('home') !== null) {
