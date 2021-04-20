dd3626031dbe57c60fdf25d2ac0fdc8fc1e67b52
Álvaro Tobar <ankaph@protonmail.com>
2021-04-20 11:17:59 -0400

    feat(webpack): add alias to assets path

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,16 @@
-const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
+const { environment } = require('@rails/webpacker');
+const { VueLoaderPlugin } = require('vue-loader');
+const path = require('path');
+const vue = require('./loaders/vue');
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin());
+environment.loaders.prepend('vue', vue);
 environment.config.merge({
   resolve: {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
+      assets: path.resolve('./app/assets'),
     },
   },
 });
-module.exports = environment
+module.exports = environment;
