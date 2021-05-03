8ed1764e5f964e449db87d7789c0af1ee7c31031
adolfrodeno <amvillalobos@uc.cl>
2021-05-03 14:59:57 -0400

    feat(validate-email): add sorteoamigosecreto reference url

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -2,7 +2,8 @@ class ProductSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
   attributes :id, :name, :price, :store_name, :image_url,
-             :link, :description, :category_id, :clicks
+             :link, :description, :category_id, :clicks,
+             :reference_url
 
   def store_name
     object.store.name
@@ -10,5 +11,9 @@ class ProductSerializer < ActiveModel::Serializer
 
   def image_url
     url_for(object.image.variant(resize: "650x650").processed)
+  end
+
+  def reference_url
+    "#{object.link}?ref=#{ENV.fetch('REFERENCE_URL')}"
   end
 end
