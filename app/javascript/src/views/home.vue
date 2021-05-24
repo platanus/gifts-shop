1544e6eaf801a8eaa738e51b8e03aecca5eacc41
Álvaro Tobar <ankaph@protonmail.com>
2021-05-24 11:09:25 -0400

    refactor(home): remove loader and category info

--- app/javascript/src/views/home.vue
+++ app/javascript/src/views/home.vue
@@ -1,40 +1,21 @@
 <template>
-  <div class="block w-full min-h-screen text-base bg-background">
+  <div class="block w-full min-h-screen text-base bg-gf-gray-light">
     <navbar />
     <modal
       :show-modal="emailModalIsOpen"
     />
     <div
       v-if="category"
-      v-show="!loading"
     >
-      <div class="py-4 m-auto bg-white sm:bg-secondary category-header">
-        <p class="flex justify-center text-2xl text-gray-800 sm:mb-4 sm:text-3xl sm:text-white">
-          <span class="hidden sm:flex">Encontramos:&nbsp; </span>
-          <span class="font-bold">{{ category.name }}</span>
-        </p>
-        <p class="justify-center hidden px-4 text-lg font-light leading-5 text-center text-white sm:flex">
-          {{ category.description }}
-        </p>
-      </div>
       <category
         :category="category"
-        v-if="category"
-        @loaded-category="loadedCategory"
-      />
-    </div>
-    <div class="loader-spinner">
-      <clip-loader
-        :loading="loading"
-        v-show="loading"
       />
     </div>
   </div>
 </template>
 
 <script>
-import ClipLoader from 'vue-spinner/src/ClipLoader.vue';
-import { mapState, mapMutations } from 'vuex';
+import { mapState } from 'vuex';
 import modal from '../components/modal-share-email.vue';
 import category from '../components/category';
 import navbar from '../components/navbar';
@@ -43,45 +24,17 @@ export default {
   name: 'HomeView',
   components: {
     category,
-    ClipLoader,
     navbar,
     modal,
   },
   computed: {
     ...mapState([
-      'products',
       'category',
-      'likes',
-      'loading',
       'emailModalIsOpen',
     ]),
   },
   mounted() {
-    this.$store.dispatch('getProducts').then(() => {
-      this.setLoading(false);
-    });
-  },
-  methods: {
-    ...mapMutations([
-      'setLoading',
-    ]),
-    loadedCategory() {
-      this.setLoading(false);
-    },
+    this.$store.dispatch('getProducts');
   },
 };
 </script>
-
-<style lang="scss">
-  .loader-spinner {
-    display: flex;
-    justify-content: center;
-    height: 42px;
-    margin-top: 25px;
-  }
-
-  .category-header {
-    width: calc(min(100%, 900px));
-  }
-
-</style>
